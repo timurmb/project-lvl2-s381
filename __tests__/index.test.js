941de6ae51478da28134ee44ae2bb05f44133050
timur meirbekov <timur.meirbekov@gmail.com>
2018-11-24 00:42:20 +0600

    step4

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -31,3 +31,19 @@ test('step3_test2', () => {
   const result = fs.readFileSync('__tests__/__fixtures__/test2result', 'utf8');
   expect(gendiff(path1, path2, format)).toBe(result);
 });
+
+test('step4_test1', () => {
+  const path1 = '__tests__/__fixtures__/test1before.ini';
+  const path2 = '__tests__/__fixtures__/test1after.ini';
+  const format = 'ini';
+  const result = fs.readFileSync('__tests__/__fixtures__/test1result', 'utf8');
+  expect(gendiff(path1, path2, format)).toBe(result);
+});
+
+test('step4_test2', () => {
+  const path1 = '__tests__/__fixtures__/test2before.ini';
+  const path2 = '__tests__/__fixtures__/test2after.ini';
+  const format = 'ini';
+  const result = fs.readFileSync('__tests__/__fixtures__/test2result', 'utf8');
+  expect(gendiff(path1, path2, format)).toBe(result);
+});
