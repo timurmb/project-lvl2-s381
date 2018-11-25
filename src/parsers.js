82e05069be680183814d00fa46fb37a3054075c8
timur meirbekov <timur.meirbekov@gmail.com>
2018-11-25 06:14:02 +0600

    step5

--- src/parsers.js
+++ src/parsers.js
@@ -1,11 +1,7 @@
 import yaml from 'js-yaml';
 import ini from 'ini';
 
-const parsers = {
-  json: JSON.parse,
-  yml: yaml.safeLoad,
-  ini: ini.parse,
-};
+const parsers = { json: JSON.parse, yml: yaml.safeLoad, ini: ini.parse };
 
 export default (format) => {
   if (!parsers[format]) throw new Error('unknown format');
